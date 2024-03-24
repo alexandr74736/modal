60036a46be251162ed88e56ed6db198b933905a6
alexandr74736 <a17230@yandex.ru>
2024-03-25 01:25:11 +0400

    update openModal, closeModal composables

--- src/composables/closeModal.ts
+++ src/composables/closeModal.ts
@@ -1,15 +1,16 @@
 import { useModalsStore } from "@/stores/modals";
 const closed = 100;
+const noScrollClass = 'global--no-scroll';
 
-interface modalInterface {
+interface Modal {
     storeName: string,
     modalTop: number,
     isMobile: boolean
 }
 
-export const closeModal = (modal: modalInterface) => {
+export const closeModal = (modal: Modal) => {
     const storeModals = useModalsStore()
     storeModals.setModalState(modal.storeName, false)
     modal.modalTop = closed;
-    if (modal.isMobile) document.body.classList.remove('global--no-scroll');
+    if (modal.isMobile) document.body.classList.remove(noScrollClass);
 }
--- src/composables/openModal.ts
+++ src/composables/openModal.ts
@@ -1,17 +1,18 @@
 import { useModalsStore } from "@/stores/modals";
 import { setModalNoScroll } from "./setModalNoScroll";
 const opened = 0;
+const noScrollClass = 'global--no-scroll';
 
-interface modalInterface {
+interface Modal {
     storeName: string,
     modalTop: number,
     isMobile: boolean
 }
 
-export const openModal = (modal: modalInterface) => {
+export const openModal = (modal: Modal) => {
     const storeModals = useModalsStore()
     storeModals.setModalState(modal.storeName, true)
     modal.modalTop = opened;
-    if (modal.isMobile) document.body.classList.add('global--no-scroll');
+    if (modal.isMobile) document.body.classList.add(noScrollClass);
     setModalNoScroll(true)
 }
